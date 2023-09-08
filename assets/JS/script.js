930ae0308d9df1669f3a626f4cf07d3d14df062c
hughes84 <134289376+hughes84@users.noreply.github.com>
2023-09-08 17:04:50 +0100

    Refactor/style:Split function into three seperate functions/Change style of cards

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,22 +1,48 @@
 const startButton = document.getElementById("start");
 const dialog = document.getElementById("usernameDialog");
 const username = document.getElementById("username");
-const cardBack = document.querySelectorAll(".back")
-
-cardBack.forEach(function(button){
-    button.addEventListener("click", function(){
-        
-    })
-})
-
-
-  dialog.showModal();
-  
-
-// Form start button closes the dialog box
-startButton.addEventListener("click", () => {
-    localStorage.setItem("username" , username.value);
-  dialog.style.display="none"
-  console.log(localStorage.getItem("username"))
+const cards = document.querySelectorAll(".card"); // Changed 'card' to 'cards' for clarity
+let firstCard = null;
+let score= 0;
+cards.forEach(function (card) {
+  card.addEventListener("click", cardFlip)
 });
-
+function cardFlip() {
+  const cardFront = this.querySelector(".front");
+  const cardBack = this.querySelector(".back");
+    if (cardFront.style.display === "none" || cardFront.style.display === "") {
+      cardFront.style.display = "flex";
+      cardBack.style.display = "none"; // Show the back when
+    }
+    checkMatch(this,cardBack,cardFront)
+  }
+function checkMatch(card,cardBack,cardFront){
+    if(firstCard != null){//if its not the first card to turn over
+      let firstCardData = firstCard.dataset.image;
+      if(firstCardData === card.dataset.image){//if cards match
+          console.log(score++)
+          card.removeEventListener("click", event)
+          firstCard =null
+          firstCardData = null
+      }else{//if cards dont match
+          setTimeout(() => {
+          cardFront.style.display = "none";
+          cardBack.style.display = "flex"; // Hide the back when showing the front
+          firstCard.addEventListener("click", cardFlip);
+          firstCard.children[0].style.display = "none";
+          firstCard.children[1].style.display = "flex"; // Hide the back when showing the front
+          firstCard = null
+          firstCardData = null
+        }, 2000);
+      }
+    }else{//if it is the first card to turn over
+      firstCard = card
+      firstCard.removeEventListener("click",cardFlip)
+    }
+  }
+// // Form start button closes the dialog box
+// startButton.addEventListener("click", () => {
+//     localStorage.setItem("username" , username.value);
+//   dialog.style.display="none"
+//   console.log(localStorage.getItem("username"))
+// });
